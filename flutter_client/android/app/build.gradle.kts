50e111e53ae9ee4de3ba73a0bce64002946b40e5
alicezate <112455320+smokingalice@users.noreply.github.com>
2026-05-06 23:01:50 +0800

    fix: remove NDK requirement to fix Android build failure

--- flutter_client/android/app/build.gradle.kts
+++ flutter_client/android/app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
 android {
     namespace = "com.jarvisagent.jarvis_client"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
